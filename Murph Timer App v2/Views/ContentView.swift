ea0f7e9f7ae7cb6327c69866274379229fd711d9
Zack Wagner <ztw305@gmail.com>
2023-01-09 15:41:48 -0600

    added new buttons

--- Murph Timer App v2/Views/ContentView.swift
+++ Murph Timer App v2/Views/ContentView.swift
@@ -11,100 +11,113 @@ struct ContentView: View {
     
     @ObservedObject var managerClass: ManagerClass = ManagerClass()
     @State var phase = 0
-    @State var titles = ["Mile 1","Calitchtenics","Mile 2", "Murph Complete, Congrats!"]
-    @State var times = [0.0, 0.0, 0.0, 0.0]
+    @State var titles = ["Murph Watch", "Mile 1","Calisthenics","Mile 2", "Murph Complete Congrats!"]
+    @State var times = [0.0, 0.0, 0.0, 0.0, 0.0]
     @State var mile1String = ""
     @State var calisthenicsString = ""
     @State var mile2String = ""
     @State var finalString = ""
-    
+    @State var backgrounds = ["plain", "running", "cal", "running", "plain"]
     
     var body: some View {
-        VStack{
-            
-            Text(titles[phase]).padding()
-            
-                
-            Text(ManagerClass.timeToString(mil: managerClass.secondElapsed))
-                .font(.largeTitle)
-                .multilineTextAlignment(.leading)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                .frame(width: .infinity)
-                
-                
-                
-                
-               
+
             
             
-            switch managerClass.mode {
-            case .stopped:
-                withAnimation{
-                    Button {
-                        managerClass.start()
-                    } label: {
-                        Image(systemName: "play.fill")
-                            .foregroundColor(.white)
-                            .font(.title)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(100)
-                    }
-                }
-            case .running:
-                withAnimation{
-                    HStack{
-                        //pause button
-                        Button {
-                            managerClass.pause()
-                        } label: {
-                            Image("PAUSE").resizable().frame(width: 100.0, height: 100.0).scaledToFill()
-                            
-                        }
-                        //continue button
-                        Button {
-                            times[phase] = managerClass.secondElapsed
-                            
-                            if phase == 0{
-                                mile1String = "Mile 1 Split: " +
-                                ManagerClass.timeToString(mil: managerClass.secondElapsed)
-                            }
-                            else if phase == 1{
-                                calisthenicsString = "Calisthenics Split: " + String(format: "%.2f", times[phase] - times[0])
-                            }
-                            else if phase == 2{
-                                mile2String = "Mile 2 Split: " + String(format: "%.2f", (times[phase] - times[1]))
-                                managerClass.finish()
-                                finalString = "Total Time: " + ManagerClass.timeToString(mil: managerClass.secondElapsed)
-                            }
-                            else {
-                                managerClass.finish()
-                                finalString = "Total Time: " + ManagerClass.timeToString(mil: managerClass.secondElapsed)
-                            }
-                            
-                            phase += 1
-                            
-                            
-                        } label: {
-                            Image("DONE").resizable().frame(width: 100.0, height: 100.0).scaledToFill()
-                            
-                        }
-                    }
-                }
+            VStack{
+                
+                Spacer()
+                
+                Text(titles[phase]).font(.largeTitle).fontWeight(.bold).multilineTextAlignment(.center).padding()
+                
+                    
+                Text(ManagerClass.timeToString(mil: managerClass.secondElapsed))
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.leading)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    
+                    
+                   
                 
                 
-                
-                
-                
-                
-            case .paused:
-                withAnimation{
-                    HStack{
+                switch managerClass.mode {
+                case .stopped:
+                    withAnimation{
                         Button {
                             managerClass.start()
                         } label: {
-                            Image("RESUME").resizable().frame(width: 100.0, height: 100.0).scaledToFill()
+                            Image("BEGIN").resizable().frame(width: 100.0, height: 100.0).scaledToFill()
                         }
+                    }
+                case .running:
+                    withAnimation{
+                        HStack{
+                            //pause button
+                            Button {
+                                managerClass.pause()
+                            } label: {
+                                Image("PAUSE").resizable().frame(width: 100.0, height: 100.0).scaledToFill()
+                                
+                            }
+                            //continue button
+                            Button {
+                                times[phase] = managerClass.secondElapsed
+                                
+                                if phase == 0{
+                                    mile1String = "Mile 1 Split: " +
+                                    ManagerClass.timeToString(mil: managerClass.secondElapsed)
+                                }
+                                else if phase == 1{
+                                    calisthenicsString = "Calisthenics Split: " + String(format: "%.2f", times[phase] - times[0])
+                                }
+                                else if phase == 2{
+                                    mile2String = "Mile 2 Split: " + String(format: "%.2f", (times[phase] - times[1]))
+                                    managerClass.finish()
+                                    finalString = "Total Time: " + ManagerClass.timeToString(mil: managerClass.secondElapsed)
+                                }
+                                else {
+                                    managerClass.finish()
+                                    finalString = "Total Time: " + ManagerClass.timeToString(mil: managerClass.secondElapsed)
+                                }
+                                
+                                phase += 1
+                                
+                                
+                            } label: {
+                                Image("DONE").resizable().frame(width: 100.0, height: 100.0).scaledToFill()
+                                
+                            }
+                        }
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                case .paused:
+                    withAnimation{
+                        HStack{
+                            Button {
+                                managerClass.start()
+                            } label: {
+                                Image("RESUME").resizable().frame(width: 100.0, height: 100.0).scaledToFill()
+                            }
+                            Button {
+                                managerClass.reset()
+                                phase = 0
+                                mile1String = ""
+                                mile2String = ""
+                                calisthenicsString = ""
+                                finalString = ""
+                            } label: {
+                                Image("RESET").resizable().frame(width: 100.0, height: 100.0).scaledToFill()
+                            }
+                        }
+                    }
+                case.finished:
+                    withAnimation{
                         Button {
                             managerClass.reset()
                             phase = 0
@@ -113,44 +126,33 @@ struct ContentView: View {
                             calisthenicsString = ""
                             finalString = ""
                         } label: {
-                            Image("RESET").resizable().frame(width: 100.0, height: 100.0).scaledToFill()
+                            Image("START OVER")
+                                .cornerRadius(10)
+                                
+                                
+                            
                         }
-                    }
-                }
-            case.finished:
-                withAnimation{
-                    Button {
-                        managerClass.reset()
-                        phase = 0
-                        mile1String = ""
-                        mile2String = ""
-                        calisthenicsString = ""
-                        finalString = ""
-                    } label: {
-                        Image(systemName: "stop.fill")
-                            .foregroundColor(.white)
-                            .font(.title)
-                            .padding()
-                            .background(Color.yellow)
-                            .cornerRadius(100)
+                        
                         
                     }
                     
-                    
                 }
-                
-            }
                 Text(mile1String)
-                Text(calisthenicsString)
-                Text(mile2String)
-                Text(finalString)
+                    .font(.title2)
+                    .frame(height: 40.0)
+                Text(calisthenicsString).font(.title2).frame(height: 40.0)
+                Text(mile2String).font(.title2).frame(height: 40.0)
+                Text(finalString).font(.title).fontWeight(.bold).frame(height: 60.0)
+                    
+                Spacer()
+                Spacer()
                 
                 
-        
+            
+            }
         }
     }
     
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
